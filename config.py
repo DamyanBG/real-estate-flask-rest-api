3ad0b6f25f23d935394c2d96029320b109626be5
DamyanBG <d.dimitrov.1988@abv.bg>
2024-03-29 11:13:04 +0200

    configuration of redis

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_caching import Cache
 
 from db import db
 from resources.routes import routes
@@ -15,7 +16,11 @@ class DevApplicationConfiguration:
         f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
         f"@{config('DB_HOST')}/{config('DB_NAME')}"
     )
-
+    CACHE_TYPE = "RedisCache"
+    CACHE_DEFAULT_TIMEOUT = 300
+    CACHE_REDIS_HOST = "localhost"
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = 0
 
 # class TestApplicationConfiguration:
 #     DEBUG = True
@@ -30,6 +35,7 @@ def create_app(config="config.DevApplicationConfiguration"):
     app = Flask(__name__)
     app.config.from_object(DevApplicationConfiguration)
     migrate = Migrate(app, db)
+    cache = Cache(app)
     CORS(app)
     api = Api(app)
     [api.add_resource(*r) for r in routes]
