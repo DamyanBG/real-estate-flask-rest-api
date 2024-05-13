547d58c09108b2efb9b7092ac713a3603c7fc15d
DamyanBG <d.dimitrov.1988@abv.bg>
2024-05-13 18:15:18 +0300

    add meeting partner names

--- resources/meeting_resource.py
+++ resources/meeting_resource.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 
 from managers.auth_manager import auth
 from managers.meeting_manager import MeetingManager
+from managers.user_manager import UserManager
 from utils.decorators import validate_schema
 from schemas.request.meeting_request import MeetingRequestSchema
 from schemas.response.meeting_response import MeetingResponseSchema
@@ -21,5 +22,9 @@ class MeetingResource(Resource):
     def get(self):
         current_user = auth.current_user()
         user_meetings = MeetingManager.select_meetings_by_user(current_user.id)
+        for user_meeting in user_meetings:
+            print(user_meeting.invited_id)
+            meeting_partner_id = user_meeting.invited_id if user_meeting.invited_id != current_user.id else user_meeting.invitor_id
+            user_meeting.meeting_partner_names = UserManager.select_user_names(meeting_partner_id)
         resp_schema = MeetingResponseSchema()
         return resp_schema.dump(user_meetings, many=True), 200
--- schemas/response/meeting_response.py
+++ schemas/response/meeting_response.py
@@ -6,3 +6,4 @@ from schemas.bases import BaseMeetingSchema
 class MeetingResponseSchema(BaseMeetingSchema):
     id = fields.Integer(required=True)
     created_on = fields.DateTime(required=True)
+    meeting_partner_names = fields.String()
